fbd4b4081c579b19f93d5aad87747676975ad25e
Zishan2211 <alljubayerzishan@gmail.com>
2023-03-28 03:25:15 +0600

    Title change and Icone Change

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Products.css'
 
 
 const Products = (props) => {
     const { img, name, price, seller, ratings } = props.products;
     const hendeladdtoCart = props.hendeladdtoCart;
-    return (    
+    return (
         <div className='Product'>
             <img src={img} alt="" />
             <div className='product-info'>
                 <h6 className='product-name'> {name}</h6>
                 <h5 className='h5'>Price: ${price}</h5>
                 <p>Manufacturer: {seller}</p>
-                <p>Rating: {ratings} stars</p> 
+                <p>Rating: {ratings} stars</p>
             </div>
 
-            <button onClick={() => hendeladdtoCart(props.products)} className='btn-cart'>Add To Cart</button>
+            <button onClick={() => hendeladdtoCart(props.products)} className='btn-cart'>Add To Cart <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };
